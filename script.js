61e11f13636b1892716b7dd5890efd3a31345e09
mrndolo <callistomutunga@gmail.com>
2024-07-10 10:25:12 +0300

    Added list visibility functionality

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var selectField = document.getElementById("selectField");
 var selectText = document.getElementById("selectText");
 var options = document.getElementsByClassName("options");
+var list = document.getElementById("list");
+
+selectField.onclick = function(){
+    list.classList.toggle("hide");
+}
 
 for(option of options){
     option.onclick = function(){
         selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
     }
 }
